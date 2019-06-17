051adb20b55ef0f67cb1387f6bbb4d00c9229364
keisuke713 <nebakei.tkb713@gmail.com>
2019-06-17 21:11:56 +0900

    create_pegination

--- app.rb
+++ app.rb
@@ -5,16 +5,34 @@ require 'byebug'
 require 'active_record'
 
 Dir[File.expand_path('../model', __FILE__) << '/*.rb'].each do |file|
-  require file
+  load file
 end
+
 
 ActiveRecord::Base.establish_connection(
   adapter: 'sqlite3',
   database: './myapp.db'
 );
 
-get '/' do
-  @title = 'all_tweets'
-  @posts = Post.all
+helpers do
+  def str_slice(text)
+    "#{text.slice(0..39)}..."
+  end
+
+  def total_page
+    (Post.count / 5) + 1
+  end
+end
+
+get %r{/posts/page/([0-9]+)} do
+  @title = 'all_posts'
+  @page_num = (params[:captures][0]).to_i
+  @posts = Post.get_post(@page_num)
   erb :index
+end
+
+get '/posts/:id' do
+  @title = 'show_post'
+  @post = Post.find(params[:id])
+  erb :show
 end
